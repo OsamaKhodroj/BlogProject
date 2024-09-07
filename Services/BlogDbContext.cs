e15c1ca00d5169502a16a1e40fcdf73f9ddcfa3b
Osama Khodrog <khodrog@hotmail.com>
2024-09-07 20:02:50 +0300

    remove unused using

--- Services/BlogDbContext.cs
+++ Services/BlogDbContext.cs
@@ -1,29 +1,23 @@
 ﻿using Entities.Domains;
 using Microsoft.EntityFrameworkCore;
 using Services.Confguration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Services
+namespace Services;
+
+public class BlogDbContext : DbContext
 {
-    public class BlogDbContext : DbContext
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
     {
-        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
-        {
-
-        }
          
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            ArticleConfiguration.ArticleConfigurationAttribute(modelBuilder);
-            CatigoryConfiguration.CatigoryConfigurationAttribute(modelBuilder);
-            SeedData.InitalSeedData(modelBuilder);
-        }
-
-        public DbSet<Article> Articles { get; set; }
-        public DbSet<Category> Categories { get; set; }
     }
+     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArticleConfiguration.ArticleConfigurationAttribute(modelBuilder);
+        CatigoryConfiguration.CatigoryConfigurationAttribute(modelBuilder);
+        SeedData.InitalSeedData(modelBuilder);
+    }
+
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Category> Categories { get; set; }
 }
